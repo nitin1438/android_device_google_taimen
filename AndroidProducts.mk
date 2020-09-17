d1bb0469338d7f8b5fbb9963655b5ff7289f9128
Lunarixus <Nathan@lunarixus.party>
2020-09-17 20:42:55 +0000

    taimen: Remove hwasan makefile
    Signed-off-by: Lunarixus <Nathan@lunarixus.party>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/hentai_taimen.mk \
-    $(LOCAL_DIR)/aosp_taimen_hwasan.mk \
+    $(LOCAL_DIR)/hentai_taimen.mk
 
 COMMON_LUNCH_CHOICES := \
     hentai_taimen-userdebug
