31f87fe37d6ff28187bdf86580780cf47f3f9c61
Lunarixus <Nathan@lunarixus.party>
2020-09-17 20:42:29 +0000

    taimen: Convert from AOSP to hentaiOS
    Signed-off-by: Lunarixus <Nathan@lunarixus.party>

--- hentai_taimen.mk
+++ hentai_taimen.mk
@@ -18,7 +18,27 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+#
+# All components inherited here go to system_ext image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
+# All components inherited here go to product image
+#
+$(call inherit-product, vendor/hentai/build/product/hentai_product.mk)
+
+#
+# All components inherited here go to vendor image
+#
+# TODO(b/136525499): move *_vendor.mk into the vendor makefile later
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
+
+# Inherit some common hentai stuff.
+$(call inherit-product, vendor/hentai/config/common_telephony.mk)
+
+# Inherit product speciifc makefiles
 $(call inherit-product, device/google/taimen/device.mk)
 $(call inherit-product-if-exists, vendor/google_devices/taimen/proprietary/device-vendor.mk)
 
@@ -39,6 +59,6 @@ PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
 
 PRODUCT_MANUFACTURER := Google
 PRODUCT_BRAND := Android
-PRODUCT_NAME := aosp_taimen
+PRODUCT_NAME := hentai_taimen
 PRODUCT_DEVICE := taimen
 PRODUCT_MODEL := AOSP on taimen
